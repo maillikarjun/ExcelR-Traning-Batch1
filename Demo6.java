5eea69832c201215d9270f05c5f11fa8d3ecbf3f
maillikarjun <mallikarjun12004@gmail.com>
2025-01-04 12:34:59 +0530

    Add files via upload

---
+++ Demo6.java
@@ -0,0 +1,11 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String name = "mallikarjun";
+        String pwd = "mallikarjun123";
+        while((name == "mallikarjun")&&(pwd == "mallikarjun123")){
+        System.out.println("Hello mallikarjuin welcome to mallareddy university");
+        break;
+        }
+    }
+    
+}
