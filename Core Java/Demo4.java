87cd160f5d3561e6b24cae363025308f6cf67304
maillikarjun <mallikarjun12004@gmail.com>
2025-02-04 21:49:22 -0800

    Delete Core Java/Demo4.java

--- Core Java/Demo4.java
+++
@@ -1,9 +0,0 @@
-public class Demo4 {
-    public static void main(String[] args){
-        int n = 100;
-        for(int i=0;i<=n;i++){
-            System.out.println(i);
-        }
-    }
-    
-}
